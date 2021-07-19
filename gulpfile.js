16f59f064be1888a43a7f102382838e47c66f572
Amnish Sthapit <amnish138@gmail.com>
2021-07-19 12:19:49 +0545

    qr

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,21 @@
 //Initialize modules
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var browserSync = require("browser-sync").create();
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
 
 function style() {
-  return gulp
-    .src("./assets/**/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("./css"))
-    .pipe(browserSync.stream());
+  return gulp.src('./assets/**/*.scss').pipe(sass()).pipe(gulp.dest('./css')).pipe(browserSync.stream());
 }
 
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "./",
+      baseDir: './',
     },
   });
-  gulp.watch("./assets/**/*.scss", style);
-  gulp.watch("./*.html").on("change", browserSync.reload);
-  gulp.watch("./assets/**/*.js").on("change", browserSync.reload);
+  gulp.watch('./assets/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./assets/**/*.js').on('change', browserSync.reload);
 }
 exports.style = style;
 exports.watch = watch;
